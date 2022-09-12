cb680feaf5f492f389a0998d6aa42bf29a01f60d
Matthew Tam <matthewethan.tam@gmail.com>
2022-09-12 11:19:28 +0200

    adding to fork

--- src/examples/DBDriverMatthewTam.java
+++ src/examples/DBDriverMatthewTam.java
@@ -4,7 +4,7 @@ public class DBDriverMatthewTam {
      * @throws DatabaseException
      */
     public static void main (String[] args) throws DatabaseException {
-            DatabaseConnectionPool Pool1 = new DatabaseConnectionPool("pool1", "jdbc:oracle:thin:@localhost:1521:orcl", "scott", "tiger", 10, 5);
+            DatabaseConnectionPool Pool1 = new DatabaseConnectionPool("pool1", "jdbc:oracle:thin:@localhost:1521:orcl", "scott", "tiger", 3, 1);
             DatabaseConnection dc1 = Pool1.acquireConnection(40);
     }
 }
